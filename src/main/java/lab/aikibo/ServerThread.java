3ab7f0750d5710b2777e27651308fc31afb39334
tamami <tamami.oka@gmail.com>
2017-03-23 15:05:15 +0700

    update content sampai run() di ServerThread

--- src/main/java/lab/aikibo/ServerThread.java
+++ src/main/java/lab/aikibo/ServerThread.java
@@ -1,7 +1,45 @@
 package lab.aikibo;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  * Created by tamami on 23/03/17.
  */
 public class ServerThread extends Thread {
+
+    public SocketServer server = null;
+    public Socket socket = null;
+    public int id = -1;
+    public ObjectOutputStream streamOut = null;
+    public ObjectInputStream streamIn = null;
+
+    public ServerThread(SocketServer server, Socket socket) {
+        super();
+        this.server = server;
+        this.socket = socket;
+        id = socket.getPort();
+        ui = server.ui;
+    }
+
+    public void send(Message msg) {
+        try {
+            streamOut.writeObject(msg);
+            streamOut.flush();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void run() {
+        public void run() {
+
+        }
+    }
 }
