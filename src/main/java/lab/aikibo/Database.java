ea4608c9773616da0fc3b7aac6a4a4e31ecd3697
tamami <tamami.oka@gmail.com>
2017-03-23 22:59:32 +0700

    update content

--- src/main/java/lab/aikibo/Database.java
+++ src/main/java/lab/aikibo/Database.java
@@ -4,13 +4,22 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.io.IOException;
 
 /**
- * Created by tamami on 23/03/17.
+ * Created by tamami on 23/03/17
  */
 public class Database {
 
@@ -44,6 +53,11 @@ public class Database {
             e.printStackTrace();
             return false;
         }
+    }
+
+    private String getTagValue(String username, Element eElement) {
+        //eElement.ElementsByTagName(username);
+        //return null;
     }
 
     public boolean checkLogin(String username, String password) {
@@ -80,7 +94,37 @@ public class Database {
 
     public void addUser(String username, String password) {
         try {
-            
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse("filePath");
+
+            Node data = doc.getFirstChild();
+
+            Element newUser = doc.createElement("user");
+            Element newUsername = doc.createElement("username");
+            newUsername.setTextContent(username);
+            Element newPassword = doc.createElement("password");
+            newPassword.setTextContent(password);
+
+            newUser.appendChild(newUsername);
+            newUser.appendChild(newPassword);
+            data.appendChild(newUser);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(filePath));
+            transformer.transform(source, result);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
         }
     }
 
--- src/main/java/lab/aikibo/Message.java
+++ src/main/java/lab/aikibo/Message.java
@@ -1,7 +1,24 @@
 package lab.aikibo;
 
+import java.io.Serializable;
+
 /**
  * Created by tamami on 23/03/17.
  */
-public class Message {
+public class Message implements Serializable {
+
+    public String type, sender, content, recipient;
+
+    public Message(String type, String sender, String content, String recipient) {
+        this.type = type;
+        this.sender = sender;
+        this.content = content;
+        this.recipient = recipient;
+    }
+
+    public String toString() {
+        return "{type='" + type + "', sender='" + sender + "', content='" + content + "', recipient='" +
+            recipient + "'}";
+    }
+
 }
